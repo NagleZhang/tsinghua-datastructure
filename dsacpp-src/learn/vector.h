2dad93c29028ec4d6441ca253324fba7775bc651
Nagle Zhang <nagle.zhang@qq.com>
2021-09-15 22:50:45 +0800

    adjacency matrix.

---
+++ dsacpp-src/learn/vector.h
@@ -0,0 +1,88 @@
+/*
+ * vector's metadata: size, capacity, data
+ * vector's functions: Create, Read, Update, Delete
+ *   Create: allocate.
+ *   Read: Get from Rank, Find some value, terverse
+ *   Update: insert, expand, shrink, sort, unsort
+ *   Delete: delete by rank;
+ */
+class Vector() {
+ public:
+  int _capacity;
+  int _size;
+  int data;
+};
+
+/*
+ * List's metadata:
+ *  List(data, headListNode, tailNode, size)
+ *  ListNode(prev, next).
+ * List's function: Create, Read, Update, Delete
+ *   Create: allocate.
+ *   Read: Get from Rank, Find some value, terverse
+ *   Update: insert, sort, unsort
+ *   Delete: delete by rank;
+ */
+
+struct ListNode ();
+struct ListNodePosi() = ListNode();
+struct ListNode () {
+ public:
+  next ListNode;
+  prev ListNode;
+  int data;
+
+  insertAsNext();
+  insertAsPrev();
+}
+class List () {
+ public:
+  ListNode head, tail;
+  int _size;
+  insertAfterNode();
+  insertBeforeNode();
+}
+
+/*
+ * can implement by vector.
+ */
+class Stack() {
+  pop();
+  push();
+
+  trim();
+}
+
+/*
+ * can implement by vector as well.
+ */
+class Queue(){
+  enqueue();
+  dequeue();
+}
+
+/*
+ * BinTree's metadata:
+ *  BinNode/BinNodePosition : leftChild, rightChild, data, Parent, height
+ *  BinTree: root, _size
+ * BinTree's function: Create, Read, Update, Delete
+ *  Create: allocate
+ *  Read: traverse, leftChildNode, rightChildNode, ParentNode
+ *  Update: insertAsRoot, insertAsLeftChild, insertAsRightChild, insertAsLeftTree, insertAsRightTree
+ *  Delete: removeNode
+ */
+struct BinNode {};
+
+class BinTree() {}
+
+/*
+ * Graph adjacency matrix impelete 邻接矩阵
+ */
+struct Vertex  {};
+
+struct Edge {};
+
+class GraphMatrix() {}
+
+class Graph() {}
+
